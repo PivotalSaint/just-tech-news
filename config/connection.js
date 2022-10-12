d4890ecce6495078ad58cbfcdafaf80d6818c43e
PivotalSaint <licensedmt@att.net>
2022-10-12 12:11:42 -0500

    trying to fix broken

--- config/connection.js
+++ config/connection.js
@@ -3,17 +3,17 @@ const Sequelize = require('sequelize');
 
 require('dotenv').config();
 
-let sequelize;
+//let sequelize;
 
-if (process.env.JAWSDB_URL) {
-    sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
+//if (process.env.JAWSDB_URL) {
+    //sequelize = new Sequelize(process.env.JAWSDB_URL);
+//} else {
     // create connection to our database, pass in your mysql info for username and password
     const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
         port: 3340
     });
-}
+//}
 
 module.exports = sequelize;
