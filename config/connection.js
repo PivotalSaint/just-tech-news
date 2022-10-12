3342cce0d1d93336fc75ef4d84face9b9b77c6f5
PivotalSaint <licensedmt@att.net>
2022-10-12 12:23:38 -0500

    fixed

--- config/connection.js
+++ config/connection.js
@@ -12,7 +12,7 @@ require('dotenv').config();
     const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3340
+        port: 3306
     });
 //}
 
